7d42e9f595a86cbfb4db2216c7205133968b7a69
hebaraajab <hragab.je@gmail.com>
2023-08-15 12:45:40 -0700

    upload 4-puts file

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,17 @@
+/*
+ * we use this line to declre the standard input and outpput functions 
+ * name: heba
+ */
+
+#include <stadio.h>
+
+/*
+ * we used the puts function for printing to print our statement
+ * we use return function to end program 
+ */
+int main(void)
+{
+	puts("Programming is like building a multilingual puzzle");
+
+	return (0);
+}
