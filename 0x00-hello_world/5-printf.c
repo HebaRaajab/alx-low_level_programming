de65877d574f54cdd5f78ffb1374cbfb45822513
hebaraajab <hragab.je@gmail.com>
2023-08-16 02:21:43 -0700

    upload 5-pintf file

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,17 @@
+/*
+ * File: 5-printf.c
+ * name:heba ragab
+ */
+
+#include <stdio.h>
+
+ /*
+  * main -printf("with proper grammar, but the outcome is a piece of art,"
+  * we use return function to end program
+  */
+	int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art, \n");
+
+	return (0);
+}
