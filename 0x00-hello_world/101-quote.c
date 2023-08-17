261137cf2a62f4b07030dd1d6f1bf54ee4b39e5e
hebaraajab <hragab.je@gmail.com>
2023-08-17 15:31:13 -0700

    upload 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,25 @@
+/*
+ * Filename: 101-quote.c
+ * Username: Heba Ragab
+ */
+#include <stdio.h>
+#include <string.h>
+#include <unistd.h>
+
+/**
+ * main - entry point
+ *
+ * Description: Using write function to print a statement.
+ *              The statement can be print directly using write(1, "text", n)
+ * Return: Always (1)
+ */
+int main(void)
+{
+
+	char myWord[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19";
+	int sLong = strlen(myWord);
+
+	write(1, myWord, sLong);
+	return (1);
+}
+
