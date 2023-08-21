119f13007ca00df261fe458773c2988a0d76b4e5
hebaraajab <hragab.je@gmail.com>
2023-08-20 19:01:37 -0700

    upload 0-positive_or_negative.c file

---
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,36 @@
+/*
+ * Filename:0-positive_or_negative.c
+ * writer: Heba Ragab
+ */
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/* more headers goes there */
+
+/* betty style doc for function main goes there */
+/**
+ * main - entry point
+ * Description: its a program which check number to
+ *		declere it type is it zero or positive
+ *		or negative.
+ *Return: Always 0.
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/* your code goes there */
+	printf("Enter a number to check:\n");
+	scanf("%i", &n);
+	if (n > 0)
+		printf("%d is positive", n);
+	else if (n == 0)
+		printf("%d is zero", n);
+	else if (n < 0)
+		printf("%d is negative", n);
+	return (0);
+
+}
