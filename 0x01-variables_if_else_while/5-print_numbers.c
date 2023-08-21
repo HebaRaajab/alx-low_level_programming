c198b2911a3842c02796102c7f528bda2dce002d
hebaraajab <hragab.je@gmail.com>
2023-08-21 09:29:01 -0700

    upload 5-print_numbers.c  file

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,21 @@
+/*
+ * Filename: 5-print_numbers.c
+ * Writer: Heba Ragab
+ */
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: print numbers from zero to ten
+ *		followed by newline.
+ * Return: Always 0
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a <= 10; a++)
+		printf("%d", a);
+	printf("\n");
+	return (0);
+
+}
