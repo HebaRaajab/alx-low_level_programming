39c7f9f3f9e895b820ad6a52c7c7f792e04fcc4c
hebaraajab <hragab.je@gmail.com>
2023-08-21 11:09:27 -0700

    Upload 7-print_tebahpla.c file

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+/*
+ * Filename:7-print_tebahpla.c
+ * writer:Heba Ragab
+ */
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description:Print alphabet in reverse
+ * Return: Always 0.
+ */
+int main(void)
+{
+	char letter;
+
+	for (letter = 'z'; letter >= 'a'; a--)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+}
