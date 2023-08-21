9293b284f7216d8a0211c91a7021c10169551701
hebaraajab <hragab.je@gmail.com>
2023-08-21 07:57:22 -0700

    upload 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+/*
+ * Filename:2-print_alphabet.c
+ * Writer: Heba Ragab
+ */
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Use for loop to make an iteration over alphabet by
+ *		putchar.
+ * Return: Always 0
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'a'; ch <= 'z'; ch++)
+		putchar(ch);
+	return (0);
+}
