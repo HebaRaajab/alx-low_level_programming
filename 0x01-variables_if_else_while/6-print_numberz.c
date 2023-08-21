c0ca36a47745441d74ce406c97f09ae13626d1c9
hebaraajab <hragab.je@gmail.com>
2023-08-21 10:49:29 -0700

    Update 6-print_numberz.c file

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,23 @@
+/*
+ * Filename:6-print_numberz.c
+ * writer:Heba Ragab
+ */
+#include <stdio.h>
+/**
+ * main - entry point
+ * Description: Printing numbers from 0 to
+ *		10 using putchar to print the output.
+ * Return: Always (0)
+ */
+
+int main(void)
+{
+	int number;
+
+	for (number = 0; number < 10; number++)
+	{
+		putchar('0' + a);
+	}
+	putchar('\n');
+	return (0);
+}
