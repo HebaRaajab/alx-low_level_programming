df2955db26a5eeeda63cf05d18111beba9e43ae2
hebaraajab <hragab.je@gmail.com>
2023-08-22 16:12:02 -0700

    upload 1-last_digit.c file

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,38 @@
+/*
+ * Filename:1-last_digit.c
+ * Writer:Heba Ragab
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - Entry point
+ * Description: print last digit of number
+ * Return: Always 0
+ */
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	int reminder;
+
+	reminder = n % 10;
+
+	if (reminder > 5)
+	{
+		printf("Last digit of %d is %d and is greater than 5\n", n, reminder);
+	}
+	else if (reminder == 0)
+	{
+		printf("Last digit of %d is %d and is equal to 0\n", n, reminder);
+	}
+	else if (reminder < 5)
+	{
+		printf("Last digit of %d is %d less than 6 and not 0\n", n, reminder);
+	}
+	return (0);
+}
+
