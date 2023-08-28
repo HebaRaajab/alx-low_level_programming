f43e2bdd0642ba0bd72afaf030b93c9b113d3cbd
HebaRaajab <hebaelshafei2001@gmail.com>
2023-08-28 14:25:55 -0700

    Upload 100-times file

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -21,18 +21,18 @@ void print_times_table(int n)
 				}
 				else if (k < 10)
 				{
-					print_delimiter_space();
+					void print_delimiter_space(void);
 					_putchar(k + '0');
 				}
 				else if (k >= 10 && k < 100)
 				{
-					print_delimiter_space();
+					void print_delimiter_space(void);
 					_putchar((k / 10) + '0');
 					_putchar((k % 10) + '0');
 				}
 				else if (k >= 100)
 				{
-					print_delimiter_space();
+					void print_delimiter_space(void);
 					_putchar((k / 100) + '0');
 					_putchar(((k / 10) % 10) + '0');
 					_putchar((k % 10) + '0');
