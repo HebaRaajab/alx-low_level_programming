11852200673d5fea2faaa41901975b894fb60af0
HebaRaajab <hebaelshafei2001@gmail.com>
2023-08-28 12:52:06 -0700

    Upload 5-sign file fun&loop

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,8 +2,8 @@
 
 /**
  * print_sign - prints the sign of a number
- * @n: int
- * Return: 1
+ * @n: the int to check
+ * Return: 1 and prints + if n is greater than zero
  * 0 and prints 0 if n is zero
  * -1 and prints - if n is less than zero
  */
@@ -13,15 +13,18 @@ int print_sign(int n)
 	{
 		_putchar('+');
 		return (1);
-	} else if (n == 0)
-}
+	}
+	else if (n == 0)
+	{
 		_putchar(48);
 		return (0);
-	} else if (n < 0)
+	}
+	else if (n < 0)
 	{
 		_putchar('-');
-	}
 		return (-1);
-}
+	}
 
+	return (0); /* This is an optional default return value */
+}
 
