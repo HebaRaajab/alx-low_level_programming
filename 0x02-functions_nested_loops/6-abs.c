4288c45263ead43309384da5970538b90727edbb
HebaRaajab <hebaelshafei2001@gmail.com>
2023-08-28 13:35:42 -0700

    Upload 6 file task fun. and loops

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - absuluate value of number
+ * @n: integer to check
+ * Return: the absolute value of int
+ */
+int _abs(int n)
+{
+	if (n >= 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
