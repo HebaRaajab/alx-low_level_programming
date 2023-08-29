5742c19ef441a27086d9dba7ae51e6e0210f99a1
HebaRaajab <hebaelshafei2001@gmail.com>
2023-08-29 11:00:20 -0700

    Upload 3 time 0-isupper file

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -8,19 +8,9 @@
 
 int _isupper(int c)
 {
-	char c;
-
-	if (c == 'A' || c == 'B' || c == 'C' || c == 'D' || c == 'E' || c == 'F' ||
-		c == 'G' || c == 'H' || c == 'I' || c == 'J' || c == 'K' || c == 'L' ||
-		c == 'M' || c == 'N' || c == 'O' || c == 'P' || c == 'Q' || c == 'R' ||
-		c == 'S' || c == 'T' || c == 'U' || c == 'V' || c == 'W' || c == 'X')
-	{
+	if (c >= 'A' && c <= 'Z')
 		return (1);
-	}
 	else
-	{
 		return (0);
-	}
 }
-
 
